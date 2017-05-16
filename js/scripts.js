8f9ba5a30fd683b77748a40fad8f516725e660e4
Brendan Hanna and Jordan Mysliwiec <brendan+jordan@>
2017-05-16 16:17:51 -0700

    add show() hide() for initially hidden div.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 // Business Logic//
+//Created Pokemon//
+var Charmander = new Pokemon("Charmander", 100, "fire");
+var Pikachu = new Pokemon("Pikachu", 100, "lightning");
+var currentGame = new Game(0);
+
 //Pokemon Constructor//
 function Pokemon (name, health, type) {
   this.pokemonName = name;
   this.health = health;
   this.pokemonType = type;
 }
-
+//Lena help
 function Game (currentNumber){
   this.currentNumber = currentNumber;
+  this.players=[];
+  this.currentPlayer;
 }
 
-//Created Pokemon//
-var Charmander = new Pokemon("Charmander", 100, "fire");
-var Pikachu = new Pokemon("Pikachu", 100, "lightning");
-var currentGame = new Game(0);
 
 
 //random number generator functions
@@ -24,6 +27,9 @@ function getRandomInt() {
 // Game.prototype.appendString = function (x, y) {
 //   return "<p> " + x + " attacks " + y + "! </p>  <p>" + y + " sustains " + Game.currentNumber + " damage.</p>"
 // };
+function charSpecial() {
+  return getRandomInt() * 2 + 9;
+};
 
 //Game over condition evaluator function//
 function gameOver(opponent) {
@@ -35,7 +41,7 @@ function gameOver(opponent) {
 
 //Pokemon attacks//
 Pokemon.prototype.pikaAttack = function (opponent) {
-  var num = getRandomInt();//set num to random number
+  var num = getRandomInt();//set num to random integer beteween 1 and 10
   opponent.health -= num;//subtract num from oppponent health
   let health = document.getElementById("pl2-health")//set local scope variable for any elements with id pl2-health
   health.value -= num;//subtract num from the health attribute of the progress element selected
@@ -79,9 +85,6 @@ Pokemon.prototype.charAttack2 = function (opponent) {
 }
 
 
-function charSpecial() {
-  return getRandomInt() * 2 + 9;
-};
 
 
 
@@ -90,32 +93,58 @@ function charSpecial() {
 
 //User Logic//
 $(function() {
+// //Lena help
+//   $("button").click(function (){//when user clicks player 1's attack 1 button...
+//   var clickId = $(this)[0].id;
+//   console.log(clickId);
+//   //Lena help ^
+//     Pikachu.pikaAttack(Charmander);//do Pikachu attack on Charmander
+//     $("#log").empty().append("<p>Pikachu attacks Charmander!</p> <p>Charmander sustains " + Game.currentNumber + " damage.</p>");//display damage done info to user in log div
+//     $("#pl2-health-number").text(Charmander.health);
+//     $(".initially-hidden").show();
+//     $("#player1col, #player2col").hide();
+//   });
+
+  //attack button click functions
   $("#pl1-att1").click(function (){//when user clicks player 1's attack 1 button...
     Pikachu.pikaAttack(Charmander);//do Pikachu attack on Charmander
     $("#log").empty().append("<p>Pikachu attacks Charmander!</p> <p>Charmander sustains " + Game.currentNumber + " damage.</p>");//display damage done info to user in log div
     $("#pl2-health-number").text(Charmander.health);
+    $(".initially-hidden").show();
+    $("#player1col, #player2col").hide();
   });
 
   $("#pl1-att2").click(function (){//when user clicks player 1's attack 1 button...
+
     Pikachu.pikaAttack2(Charmander);//do Pikachu attack on Charmander
     $("#log").empty().append("<p>Pikachu attacks Charmander!</p> <p>Charmander sustains " + Game.currentNumber + " damage.</p>");//display damage done info to user in log div
     $("#pl2-health-number").text(Charmander.health);
+    $(".initially-hidden").show();
+    $("#player1col, #player2col").hide();
 
   });
-
 
   $("#pl2-att1").click(function (){//when user clicks player 2's attack 1 button...
     Charmander.charAttack(Pikachu);//do Charmander attack on Pikachu
     $("#log").empty().append("<p>Charmander attacks Pikachu!</p> <p>Pikachu sustains " + Game.currentNumber + " damage.</p>");//display damage done info to user in log div
     $("#pl1-health-number").text(Pikachu.health);
+    $(".initially-hidden").show();
+    $("#player1col, #player2col").hide();
+
   });
 
   $("#pl2-att2").click(function (){//when user clicks player 1's attack 1 button...
     Charmander.charAttack2(Pikachu);//do Pikachu attack on Charmander
     $("#log").empty().append("<p>Chamander attacks Pikachu!</p>  <p>Pikachu sustains " + Game.currentNumber + " damage.</p>");//display damage done info to user in log div
     $("#pl1-health-number").text(Pikachu.health);
+    $(".initially-hidden").show();
+    $("#player1col, #player2col").hide();
+
   });
 
-
+  $(".initially-hidden").click(function() {
+    $("#player1col, #player2col").show();
+    $(".initially-hidden").hide();
+  });
 
 });
