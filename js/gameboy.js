d2187052002d2eca158f5ee79642a228ceb5189c
Daniel <dlopez6877@gmail.com>
2017-05-21 17:37:58 -0700

    setup gameboy shell w/o meida queries

--- js/gameboy.js
+++ js/gameboy.js
@@ -1,7 +1,5 @@
 
 var audio = new Audio('sounds/startup.mp3');
-var battle = new Audio('sounds/battle.mp3');
-battle.loop = true;
 
 $(function() {
   $(".wrapper").click(function() {
@@ -15,7 +13,7 @@ $(function() {
       $("#nintendo-screen").delay(1000).fadeOut(3000);
       $("#logo-screen").delay(10000).fadeIn(3000);
       $("#logo-screen").fadeOut(3000, function() {
-        battle.play();
+        
       });
       $("#play-screen").delay(17000).fadeIn(3000);
 
